5824a9dea9bf25ac7bdb0eab3054aae8a2c926f0
Фаниль Азмагулов <fanilazmagulov@Mac-mini-Fanil.local>
2021-04-01 00:47:03 +0500

    Добавил проверку на массив

--- 1. Basics/tasks/falsy.test.js
+++ 1. Basics/tasks/falsy.test.js
@@ -6,8 +6,20 @@ test('Falsy-значения удаляются из непустого масс
 	const newArray = [];
 
 	// Act 
-	let result = new TestForLies(testArray, newArray);
+	const result = new TestForLies(testArray, newArray);
 
 	// Assert
 	expect(result.removeFalsyValues()).toBe(TestForLies.newArray);
+});
+
+test('Аргумент является массивом', () => {
+	// Arrange
+	const testArray = [45, '', 0, 'hello', 4.7, false];
+	const newArray = [];
+
+	// Act 
+	const result = new TestForLies(testArray, newArray);
+
+	// Assert
+	expect(Array.isArray(result.newArray)).toBe(Array.isArray([]));
 });
